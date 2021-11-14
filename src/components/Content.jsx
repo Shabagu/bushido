b7a3ab14ae8a482e6bc9edc420826c5f40354361
Shabagu <93148322+Shabagu@users.noreply.github.com>
2021-11-14 14:24:13 +0300

    15. Preliminary SCSS refactoring

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -45,9 +45,9 @@ function Content() {
 function Post() {
   return(
     <div className={s.post}>
-      <div className={s.post_author}>
-        <img className={s.post_author_avatar} src="https://i.pinimg.com/736x/59/ac/de/59acde8a2e5791c5f2d2610277ed95f6--twitter-green.jpg" alt="User" />
-        <div className={s.post_author_nickname}>Nickname</div>
+      <div className={s.author}>
+        <img className={s.avatar} src="https://i.pinimg.com/736x/59/ac/de/59acde8a2e5791c5f2d2610277ed95f6--twitter-green.jpg" alt="User" />
+        <div className={s.nickname}>Nickname</div>
       </div>
       <div className={s.post_content}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias voluptas iure fuga pariatur aspernatur illo inventore, sunt dicta eligendi culpa atque amet repudiandae dolore vitae incidunt a ipsum nulla ea.
